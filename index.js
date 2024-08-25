37293e2b39f62f59ed72a2f86b03712d3cce9490
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-08-25 14:25:23 -0500

    core component로 expand 추가.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import Expand, { ExpandComponentName } from "./components/core/Expand.js";
 import Introduction, { IntroductionComponentName } from "./components/Introduction.js";
 import Experience, { ExperienceComponentName } from "./components/Experience.js";
 import Project, { ProjectsComponentName } from "./components/Projects.js";
+
+customElements.define(ExpandComponentName, Expand);
 
 customElements.define(IntroductionComponentName, Introduction);
 customElements.define(ExperienceComponentName, Experience);

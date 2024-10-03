90d058e139e9c36a239b8eb648d888bbce21ae70
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-10-02 22:26:16 -0500

    일단 색상 확정.

--- constants/color.js
+++ constants/color.js
@@ -1,3 +1,4 @@
-export const Background = "#7D5A50";
-export const Bright = "#FCDEC0";
+export const Background = "#393E46";
+export const PointColor = "#00ADB5";
+export const Bright = "#EEEEEE";
 export const WHITE = "#FFFFFF";

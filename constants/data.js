7fd5de6c05a947db41c09089f385a477eacf1cd3
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-11-10 14:57:43 -0600

    Update introduction sentence.

--- constants/data.js
+++ constants/data.js
@@ -1,7 +1,7 @@
 export const INTRODUCTION_TITLE = "Hi! I’m Muil Yang ✨";
 
 export const INTRODUCTIONS = [
-  "Master's candidate in Computer Science (expected May 2026) with over 3 years of experience in software engineering, specializing in complex, scalable web applications, system optimization, and cloud technologies.",
+  "Master's candidate in Computer Science (expected May 2026) with over 3 years of experience in software engineering, specializing in developing complex, scalable web applications, optimizing systems, and leveraging cloud technologies.",
   "I am proficient in JavaScript/TypeScript, Python, Java, Next.js/React.js, Node.js, Django, Docker/Kubernetes and AWS services.",
 ];
 

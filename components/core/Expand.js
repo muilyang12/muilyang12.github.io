03b8b8c298663ebf3295c216a891748aaeaff5b0
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2025-02-27 00:03:01 -0600

    Add media css to Expand and Link components for mobile.

--- components/core/Expand.js
+++ components/core/Expand.js
@@ -16,7 +16,7 @@ export default class Expand extends HTMLElement {
     this.shadowRoot.innerHTML = `
         <details>
             <summary>
-              <svg width="15" height="15" fill="#000000" height="800px" width="800px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve">
+              <svg fill="#000000" height="800px" width="800px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve">
                 <g>
                   <path fill="${Background}" d="M16,25c-0.6,0-1.1-0.2-1.6-0.6l-9.4-9.3c-0.7-0.7-1-1.6-1-2.5c0-1,0.4-1.8,1-2.5c1.4-1.4,3.6-1.4,5,0l5.8,5.7
                     c0.1,0.1,0.2,0.1,0.3,0l5.8-5.7c1.4-1.4,3.6-1.4,5,0c0.7,0.7,1,1.6,1,2.5c0,1-0.4,1.8-1,2.5l-9.4,9.3C17.1,24.8,16.6,25,16,25z"/>
@@ -32,6 +32,11 @@ export default class Expand extends HTMLElement {
         <style>
           details {
             border: none;
+          }
+
+          svg {
+            width: 15px;
+            height: 15px;
           }
 
           details[open] svg {
@@ -51,6 +56,19 @@ export default class Expand extends HTMLElement {
             color: ${Background};
 
             cursor: pointer;
+          }
+
+          @media (max-width: 576px) {
+              svg {
+                  width: 10px;
+                  height: 10px;
+              }
+
+              summary {
+                  padding: 2px 4px;
+
+                  gap: 2px;
+              }
           }
         </style>
     `;
